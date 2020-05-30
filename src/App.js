da9416ab77008f263e27c57f524573d7de94063c
Sacha Sedriks <sachased@gmail.com>
2020-05-30 13:57:44 -0700

    cut.n.paste continued...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 import AddBookmark from './AddBookmark/AddBookmark';
+import BookmarkApp from './BookmarkApp/BookmarkApp';
+
+const bookmarks = [
+  {
+  title:"Google",
+  url:"http://www.google.com", 
+  rating:"3", 
+  description:"No evil"
+  },
+  {
+    title:"Google",
+    url:"http://www.google.com", 
+    rating:"3", 
+    description:"No evil"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <AddBookmark />
+      <BookmarkApp bookmarks={bookmarks}/>
     </div>
   );
 }
