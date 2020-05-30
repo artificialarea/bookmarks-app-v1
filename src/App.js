435331d39c971ad69685cede1ba1b14d3dcdc28f
Sacha Sedriks <sachased@gmail.com>
2020-05-30 13:53:33 -0700

    cut.n.pasted all components from src repo static version branch.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AddBookmark from './AddBookmark/AddBookmark';
 
 function App() {
   return (
     <div className="App">
-      
+      <AddBookmark />
     </div>
   );
 }
